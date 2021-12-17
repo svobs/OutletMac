72bc7f395783ee8c5b1d5502057428ed57ccfcb3
Matt Svoboda <matt.svoboda@gmail.com>
2021-12-16 21:19:36 -0800

    Add comment. Code compiles & runs again!

--- OutletMac/Model/MenuItemMeta.swift
+++ OutletMac/Model/MenuItemMeta.swift
@@ -5,12 +5,15 @@
 
 import Foundation
 
+/*
+ Container for metadata describing the appearance and behavior of an item in a context menu. These can be nested to describe submenus.
+ */
 class MenuItemMeta: CustomStringConvertible {
     let itemType: MenuItemType
     let title: String
-    let actionID: ActionID
-    var targetGUIDList: [GUID] = []
-    var submenuItemList: [MenuItemMeta]
+    let actionID: ActionID  // can be handled (A) either here in the FE, or (B) if targetGUIDList is non-empty, sent back to the BE for processing
+    var targetGUIDList: [GUID] = []  // if the BE wants to handle the given action, it will populate this so that it can be sent back to the BE
+    var submenuItemList: [MenuItemMeta]  // if empty, this menu item is not a submenu
 
     init(itemType: MenuItemType, title: String, actionID: ActionID) {
         self.itemType = itemType
